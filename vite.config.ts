fed659a078f6ca78e6e135e59473d7f603493d5b
datapatient98-hub <datapatient98@gmail.com>
2026-01-21 14:13:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/lovable-project/", // 👈 مهم جدًا (اسم الريبو)
   server: {
     host: "::",
     port: 8080,
